224446ef14f5a73bb7d64a49d88886e1d1440553
dreamlike-ocean <56702218+dreamlike-ocean@users.noreply.github.com>
2022-01-23 16:26:02 +0800

    1,修改错误实现 EchoClientHandle.java不应该是抽象的

--- src/main/java/netty/server/EchoClientHandle.java
+++ src/main/java/netty/server/EchoClientHandle.java
@@ -8,15 +8,17 @@ import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.util.CharsetUtil;
 
 @ChannelHandler.Sharable
-public abstract class EchoClientHandle extends SimpleChannelInboundHandler<ByteBuf>  {
+public class EchoClientHandle extends SimpleChannelInboundHandler<ByteBuf>  {
 
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         ctx.writeAndFlush(Unpooled.copiedBuffer("Netty rocks!", CharsetUtil.UTF_8));
     }
 
 
+    @Override
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, ByteBuf byteBuf) throws Exception {
 
-
+    }
 
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
         cause.printStackTrace();
